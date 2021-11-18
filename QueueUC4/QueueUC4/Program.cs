b164e9d7288054a7ae30fb1c84edcdf2cc7bf614
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-18 18:08:04 +0530

    Add Uc4

---
+++ QueueUC4/QueueUC4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> s1 = new Queue<int>();
+
+            s1.Enqueue(56);
+            s1.Enqueue(30);
+            s1.Enqueue(70);
+            foreach (var ele in s1)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("--------------");
+            s1.Dequeue();
+            foreach (var ele in s1)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
