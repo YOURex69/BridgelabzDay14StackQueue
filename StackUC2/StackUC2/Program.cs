b2ccbdeca915a773b8a42da21084679aa807f942
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-18 18:06:19 +0530

    Add Uc2

---
+++ StackUC2/StackUC2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s1 = new Stack<int>();
+
+            s1.Push(56);
+            s1.Push(30);
+            s1.Push(70);
+            foreach (var ele in s1)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("Aafter---------------");
+            while(s1.Count!=0)
+            {
+                Console.WriteLine(s1.Peek());
+                s1.Pop();
+
+            }
+        }
+    }
+}
