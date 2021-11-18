d92ce4beaf8d53c46f0e109d83a3992197036ecf
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-18 18:05:19 +0530

    Add uc1

---
+++ StackUC1/StackUC1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s1 = new Stack<int>();
+
+            s1.Push(56);
+            s1.Push(30);
+            s1.Push(70);
+            foreach(var ele in s1)
+            {
+                Console.WriteLine(ele);
+            }
+
+        }
+    }
+}
